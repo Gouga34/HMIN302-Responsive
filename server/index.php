37c612f778bf7ab4e8eecd5bfcebf098731d8d94
Manouel <manuel.cha34@gmail.com>
2016-01-26 13:51:43 +0100

    Utilisation du mot en get + correction ouverture du fichier

--- server/index.php
+++ server/index.php
@@ -3,7 +3,7 @@
 header('Access-Control-Allow-Origin: *');
 require_once('parser/parser.php');
 
-//$word = $_GET['word'];
-$content = file_get_contents("http://www.jeuxdemots.org/rezo-xml.php?gotermsubmit=Chercher&gotermrel=chat&output=onlyxml");
+$word = $_GET['word'];
+$content = file_get_contents("http://www.jeuxdemots.org/rezo-xml.php?gotermsubmit=Chercher&gotermrel=".$word."&output=onlyxml");
 
 parse($content);
