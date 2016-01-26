9d0c7fbb5e2d0d7d7b292e2101d904c08d35a9b3
Gouga34 <imala34@hotmail.fr>
2016-01-26 08:56:17 +0100

    client + reprise architecture

--- assets/js/parser.js
+++ assets/js/parser.js
@@ -3,13 +3,12 @@ function getWordDescription() {
     var word = document.getElementById("searchedWord").value;
 
     $.ajax({
-        url: 'http://localhost/Responsive/server/index.php',
+        url: 'http://localhost/responsive/server/index.php',
         type: 'GET',
         data: 'word=' + word,
 
         success: function(data) {
             $("#ba").text(data);
-            alert(data);
         },
         error: function(data) {
             alert("erreur : " + data);
